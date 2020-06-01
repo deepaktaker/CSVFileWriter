730766c67878a3924182a6e579bec71c6f6bc861
i323559 <deepak.mp@sap.com>
2020-06-01 16:53:38 +0530

    Second Commit
    Change-Id: I63f592d29f9fbc5f988560655a7debaf24770944

--- MLCSVWriter/src/main/java/com/csv/writer/main/CSVWriter.java
+++ MLCSVWriter/src/main/java/com/csv/writer/main/CSVWriter.java
@@ -3,10 +3,17 @@ package com.csv.writer.main;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class CSVWriter {
     public static void main(String[] args) throws IOException {
-        try (BufferedWriter bufWrite = new BufferedWriter(new FileWriter("Data.csv"));) {
+        Path path = Paths.get("Data.csv");
+        if (args != null && args.length > 0) {
+            path = Paths.get(args[0] + System.lineSeparator() + path.toString());
+        }
+        System.out.println("Path that is set :" + path);
+        try (BufferedWriter bufWrite = new BufferedWriter(new FileWriter("src/main/resources/Data.csv"));) {
             bufWrite.write("Name,Dept,Salary\n");
             bufWrite.write("Deep,CPI,12000");
             bufWrite.flush();
